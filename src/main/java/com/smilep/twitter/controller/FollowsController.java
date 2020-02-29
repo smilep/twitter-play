3a50799d3dd09d965607ff17558af1b9b3989192
Smile <smile.pabby@gmail.com>
2020-02-29 19:04:01 +0530

    Added API documentation and handled @ in the handle names

--- src/main/java/com/smilep/twitter/controller/FollowsController.java
+++ src/main/java/com/smilep/twitter/controller/FollowsController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smilep.twitter.model.FollowersResponse;
 import com.smilep.twitter.service.FollowersService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping(path = "/follows")
 public class FollowsController {
@@ -17,7 +20,11 @@ public class FollowsController {
     private FollowersService followersService;
 
     @GetMapping
-    public FollowersResponse isXFollowsY(@RequestParam("x") String x, @RequestParam("y") String y) {
+    @ApiOperation(value = "Is handle X following handle Y?")
+    public FollowersResponse isXFollowsY(@RequestParam("x") @ApiParam(required = true, value = "Twitter Handle X") String x,
+            @RequestParam("y") @ApiParam(required = true, value = "Twitter Handle Y") String y) {
+        x = x.charAt(0) == '@' ? x.replaceFirst("@", "") : x;
+        y = y.charAt(0) == '@' ? y.replaceFirst("@", "") : y;
         return followersService.isXFollowsY(x, y);
     }
 

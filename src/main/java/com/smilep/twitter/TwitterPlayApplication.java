a32c5505506d0dd4b84a4ffb32d225a51b127c46
Smile <smile.pabby@gmail.com>
2020-03-01 09:49:00 +0530

    Updated swagger docs

--- src/main/java/com/smilep/twitter/TwitterPlayApplication.java
+++ src/main/java/com/smilep/twitter/TwitterPlayApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +28,19 @@ public class TwitterPlayApplication {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.smilep.twitter.controller"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+    
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Twitter Play")
+                .description("Developed with ❤️ by Smile")
+                .contact(new Contact("Smile", "", "smile.is.my.name@gmail.com"))
+                .build();
     }
 
 }

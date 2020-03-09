2f1853c4509a2285856c22e4582519ecbbecf285
Smile <smile.pabby@gmail.com>
2020-03-09 11:41:32 +0530

    Added CORS headers

--- src/main/java/com/smilep/twitter/controller/FollowsController.java
+++ src/main/java/com/smilep/twitter/controller/FollowsController.java
@@ -1,6 +1,7 @@
 package com.smilep.twitter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/follows")
 public class FollowsController {
 

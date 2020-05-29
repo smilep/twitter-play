262b938eed759c01bb1a8f08285713c8c8d75c0a
smilep <smile.pabby@gmail.com>
2020-05-29 08:53:54 +0530

    Pass more query params in junit

--- src/test/java/com/smilep/twitter/PlayTest.java
+++ src/test/java/com/smilep/twitter/PlayTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,15 +33,19 @@ public class PlayTest {
     public void getTweet() {
         Map<String, String> requestParams = new HashMap<>();
         String id = "1263213348000325633";
+        // Pass all request params for header generation
         requestParams.put("id", id);
+        requestParams.put("tweet_mode", "extended");
         String url = "https://api.twitter.com/1.1/statuses/lookup.json";
         String header = generator.generateHeader(HttpMethod.GET.name(), url, requestParams);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", header);
         HttpEntity<String> httpEntity = new HttpEntity<String>("body", headers);
-        // String concatenation is not good for passing in GET params. Use better approach.
-        ResponseEntity<String> response = restTemplate.exchange(url + "?id=" + id, HttpMethod.GET, httpEntity,
-                String.class);
+        // Pass all request params in the request
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("id", id)
+                .queryParam("tweet_mode", "extended");
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, String.class);
         String responseBody = response.getBody();
         assertNotNull(responseBody);
         System.out.println(responseBody);
